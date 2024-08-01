02478396ee5e427a467a192c8e2dfe8d3193d9e4
Felix frank <91718298+Gigstaroche@users.noreply.github.com>
2024-08-01 11:12:27 +0300

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,11 @@
+// Create variables of different data types
+let str = "Hello";
+let num = 42;
+let bool = true;
+let nul = null;
+let und = undefined;
+let obj = { key: "value" };
+let sym = Symbol("symbol");
+
+// Print variables to console
+console.log(str, num, bool, nul, und, obj, sym);
